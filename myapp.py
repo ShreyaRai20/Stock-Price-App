73563768e05da85b5f94e8061d36b7176f82b30e
shreya200199 <86977802+shreya200199@users.noreply.github.com>
2024-08-13 13:54:09 -0500

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -9,12 +9,16 @@ st.write("""
 shown are the stock ***Closing Price*** and ***Volume*** of google!
 """)
 
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-cOdeldf17e75
+#define the ticker symbol
 tickerSymbol = 'AAPL'
 
+#get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 
+#get the historical prices for this ticker
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end = '2020-5-31')
-
+# Open High Low Close Volume Dividends Stock Splits
 
 st.write("""
         ***Closing Price***
